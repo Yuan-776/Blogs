384813364ccc448cc97daa0f713d15c5c4f50682
Yuan Gao <ygao@my.wctc.edu>
2024-10-07 23:51:25 -0500

    1.0

---
+++ Models/DataContext.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+public class DataContext : DbContext
+{
+  public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+  
+  public DbSet<Blog> Blogs { get; set; }
+  public DbSet<Post> Posts { get; set; }
+    public object Categories { get; internal set; }
+
+    public void AddBlog(Blog blog)
+  {
+    this.Add(blog);
+    this.SaveChanges();
+  }
+    public void DeleteBlog(Blog blog)
+  {
+    this.Remove(blog);
+    this.SaveChanges();
+  }
+    public void AddPost(Post post)
+  {
+    this.Add(post);
+    this.SaveChanges();
+  }
+    public void DeletePost(Post post)
+  {
+    this.Remove(post);
+    this.SaveChanges();
+  }
+}
